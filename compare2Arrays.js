6c862600938bf95996860758137221914da7e3cd
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2019-05-08 16:24:10 +0300

    Updated compare func

--- compare2Arrays.js
+++ compare2Arrays.js
@@ -3,7 +3,7 @@ function compare(arr1, arr2) {
 
   let result;
 
-  arr1.forEach((e1, i) => arr2.forEach(e2 => {
+  arr1.forEach(e1 => arr2.forEach(e2 => {
     if (e1.length > 1 && e2.length) {
       result = compare(e1, e2);
     } else if (e1 !== e2) {
