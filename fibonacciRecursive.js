fc753101fb2d08c6d9a8cbe17607e4a265e6800d
Evgeniy Aksyonov <evgeniy.aksyonov@gmail.com>
2022-01-25 14:13:46 +0200

    Updated fibonacci recursive to ES6 one-liner

--- fibonacciRecursive.js
+++ fibonacciRecursive.js
@@ -1,9 +1,3 @@
-function fib(n) {
-  if (n < 2) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  }
-}
+const fib = (n) => (n < 2) ? n : (fib(n - 1) + fib(n - 2));
 
 console.log(fib(7));
